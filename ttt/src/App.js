6a63f5d63132fff95fb9a15483fb59b409184d86
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-06-29 19:19:30 +0545

    Update App.js

--- ttt/src/App.js
+++ ttt/src/App.js
@@ -3,12 +3,20 @@ import {useState} from 'react'
 
 function App() {
   const [turn , change] = useState('x')
-  const handle = () =>{
+  const [cell, setcell] = useState((Array(9).fill('')));
+
+  const handle = (num) =>{
+    let squares = [...cell]
     if (turn === 'x'){
+      squares[num] = 'x'
       change(turn => turn = 'o')
     }else{
+      squares[num] = 'o'
       change(turn => turn = 'x')
     }
+
+    setcell(squares)
+    console.log(squares)
   }
 
   return (
@@ -18,19 +26,19 @@ function App() {
           <tbody>
             <tr>
               
-              <td onClick={()=>handle()}>{turn}</td>
-              <td onClick={()=>handle()}>{turn}</td>
-              <td onClick={()=>handle()}>{turn}</td>
+              <td onClick={()=>handle(0)}>{cell[0]}</td>
+              <td onClick={()=>handle(1)}>{cell[1]}</td>
+              <td onClick={()=>handle(2)}>{cell[2]}</td>
             </tr>
             <tr>
-              <td onClick={()=>handle()}>{turn}</td>
-              <td onClick={()=>handle()}>{turn}</td>
-              <td onClick={()=>handle()}>{turn}</td>
+              <td onClick={()=>handle(3)}>{cell[3]}</td>
+              <td onClick={()=>handle(4)}>{cell[4]}</td>
+              <td onClick={()=>handle(5)}>{cell[5]}</td>
             </tr>
             <tr>
-              <td onClick={()=>handle()}>{turn}</td>
-              <td onClick={()=>handle()}>{turn}</td>
-              <td onClick={()=>handle()}>{turn}</td>
+              <td onClick={()=>handle(6)}>{cell[6]}</td>
+              <td onClick={()=>handle(7)}>{cell[7]}</td>
+              <td onClick={()=>handle(8)}>{cell[8]}</td>
             </tr>
           </tbody>
         </table>
